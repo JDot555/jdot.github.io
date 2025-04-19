09b326b13bc4c477e42808be3e203b182a4fa086
JDot <jdot555555100@gmail.com>
2025-04-19 17:01:33 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+import * as THREE from 'node_modules/three';
+import { PointerLockControls } from 'node_modules/three/examples/jsm/controls/PointerLockControls.js';
 
 let scene, camera, renderer, controls;
 let treasureCount = 0;
